2a7b6427160b7baa74efe5c659a0bf6adbb27534
Christian Walde <walde.christian@googlemail.com>
2015-02-20 15:16:12 +0100

    add a thanks section

--- lib/Microidium.pm
+++ lib/Microidium.pm
@@ -11,6 +11,50 @@ This has a number of dependencies that should all be available from CPAN. Run
 C<perl Makefile.PL> to get a list of dependencies that you still need to
 install.
 
+=head1 THANKS
+
+Thanks go to these people, in no particular order, for their respective
+contributions. I probably forgot a bunch. I'll add more as they come to mind, or
+you can just poke me.
+
+L<Vlambeer|http://www.vlambeer.com/> for making
+L<Luftrausers|http://luftrausers.com/> and inspiring me to start this project,
+as well as for talking openly about their
+L<development process|https://www.youtube.com/watch?v=AJdEqssNZ-U>.
+
+L<Getty|https://metacpan.org/author/GETTY> for the name.
+
+L<SVatG|http://demogroup.vc/> for emotional support and graphics programming
+knowledge.
+
+L<Paul Evans|https://metacpan.org/author/PEVANS> and other maintainers
+L<Async::IO>, used for networking.
+
+L<Ingy döt Net|https://metacpan.org/author/INGY> and other maintainers for
+L<Inline::Module> and L<Inline::C>, used to provide slow Perl math as blazing
+fast XS.
+
+L<Chris Marshall|https://metacpan.org/author/CHM> and other maintainers for
+L<OpenGL>, used for graphics, and L<PDL>, used for matrix math.
+
+L<Tobias Leich (FROGGS)|https://metacpan.org/author/FROGGS> and other
+maintainers for L<SDL>, used to handle window manager chrome, interactivity and
+audio.
+
+L<Etay Meiri|http://ogldev.atspace.co.uk/> and
+L<Jason L. McKesson|http://www.arcsynthesis.org/gltut/> for writing OpenGL
+tutorials that helped me start this off in a modern way.
+
+L<Nathan Sweet|https://github.com/NathanSweet> for his Java library
+L<kyronet|https://github.com/EsotericSoftware/kryonet> which i used as an
+inspiration for the networking parts.
+
+The L<Starsiege: Tribes|http://en.wikipedia.org/wiki/Starsiege:_Tribes> and
+L<Halo: Reach|http://en.wikipedia.org/wiki/Halo:_Reach> development teams for
+publicly talking about and explaining their respective networking models:
+L<The Tribes Networking Model|http://gamedevs.org/uploads/tribes-networking-model.pdf>
+and L<I Shot You First!|http://www.gdcvault.com/play/1014345/I-Shot-You-First-Networking>
+
 =head1 RESOURCES
 
 Sounds made with http://www.bfxr.net/
