8fd8443f0ff23ce1eff459e39a6904601e921d2c
Christian Walde <walde.christian@googlemail.com>
2015-02-20 05:07:54 +0100

    change colors to make the fliers equally visible

--- lib/Microidium/ClientRole.pm
+++ lib/Microidium/ClientRole.pm
@@ -33,7 +33,7 @@ has in_network_game   => ( is => 'rw' );
 has local_player_id   => ( is => 'rw' );
 has network_player_id => ( is => 'rw' );
 has team_colors =>
-  ( is => 'ro', default => sub { { 1 => [ 1, 1, 1, 1 ], 2 => [ .2, .2, 1, 1 ], 3 => [ .2, 1, .2, 1 ] } } );
+  ( is => 'ro', default => sub { { 1 => [ .9, .9, .9, 1 ], 2 => [ .9, .7, .2, 1 ], 3 => [ .2, .8, 1, 1 ] } } );
 
 1;
 
